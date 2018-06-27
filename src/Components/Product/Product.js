0800fc7231241c7092b8ea2bdb2626363a7991f8
PhamSinh <sinhpv86@gmail.com>
2018-06-27 17:02:43 +0700

    udae

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -14,7 +14,7 @@ export default class Product extends Component {
         return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
     }
     componentDidMount () {
-        fetch('https://api.github.com/users')
+        fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(json => {
                 this.setState({
@@ -29,32 +29,31 @@ export default class Product extends Component {
   render() {
      const {users} = this.state
     return (
-        <div className="row">
-            <div className="col-lg-9 my-4">
-                <div className="section_title">
-                    <h2>
-                        <Link className="nav-link" to="/shop">
-                            Đồ phượt
-                        </Link>
-                    </h2>
+        <div className="row bg-white">
+            <div className="row col-sm-12">
+                <div className="col-sm-9 my-4">
+                    <div className="section_title">
+                        <h2>
+                            <Link className="nav-link" to="/shop">
+                                Đồ phượt
+                            </Link>
+                        </h2>
+                    </div>
+                    { users ? (
+                        users.map(user => (
+                            <Item data= {user} key={user.id} >
+                                {user.title || '[No account]'}
+                            </Item>
+                        ))
+                    ) : (
+                            <div>Loading </div>
+                        )
+                    }
                 </div>
-                <div className="row">
-                   { users ? (
-                    users.map(user => (
-                           <Item data= {user} key={user.id} >
-                            {user.login || '[No account]'}
-                           </Item>
-                       ))
-                   ) : (
-                           <div>Loading </div>
-                       )
-                   }
+                {/* /.col-lg-9 */}
+                <div className="col-sm-3 my-4">
+                    <Sidebar>Bộ lọc</Sidebar>
                 </div>
-                {/* /.row */}
-            </div>
-            {/* /.col-lg-9 */}
-            <div className="col-lg-3 my-4">
-                   <Sidebar>Bộ lọc</Sidebar>
             </div>
         </div>
       
